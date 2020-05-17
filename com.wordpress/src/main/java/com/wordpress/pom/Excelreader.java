7ce03f36f3e31df188928ec96d95a4a9526154fc
Abhi Jani <Abhi Jani@jani>
2020-05-17 00:22:18 -0700

    Created testNG data provider function

--- com.wordpress/src/main/java/com/wordpress/pom/Excelreader.java
+++ com.wordpress/src/main/java/com/wordpress/pom/Excelreader.java
@@ -7,14 +7,14 @@ public class Excelreader {
 	
 	public static String sheetname="Sheet1";
 	
-	public static void main(String[] args) throws Exception {
+	
+	public Object[][] createpost() throws Exception
+	{
+		Object data[][]=getdata(path, sheetname);
+		return data;
 		
-		getdata(path, sheetname);
 	}
-	
-	
-	public static void getdata(String path, String sheetname)throws Exception {
-		
+	public static Object[][] getdata(String path, String sheetname)throws Exception {
 		
 		
 		 ExcelUtill utill= new ExcelUtill(path,sheetname);
@@ -22,13 +22,20 @@ public class Excelreader {
 		 int rows=utill.total_rows();
 		 int column=utill.total_col();
 		 
+			Object data[][]=new Object[rows-1][column];
+
 		 for(int i=1;i<rows;i++)
 		 {
 			 for(int j=0;j<column;j++)
 			 {
-				 System.out.println(utill.celldata(i, j));
+				 //System.out.println(utill.celldata(i, j));
+				 
+				 String celldata=utill.celldata(rows, column);
+				 
+				 data[i-1][j]=celldata;
 			 }
 		 }
+		return data;
 		
 	}
 
